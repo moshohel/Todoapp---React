17ef978036f93ab6e5e653862f825dc191193227
raahinn <rahinn11@gmail.com>
2020-09-20 18:37:21 +0600

    setting redux & mapping state to props

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import { Link } from 'react-router-dom'
+import pokeball from '../pokeball.png'
+import { connect } from 'react-redux'
 
 class Home extends Component {
-    state = {
-        posts: []
-    }
+    // state = {
+    //     posts: []
+    // }
+
     // life cycle hook can only used by class based component
     // get external data by componentDidMount using axios
     // to get external data can also can use fatch
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(res => {
-                this.setState({
-                    posts: res.data.slice(0, 5)
-                })
-            })
-    }
+
+    // componentDidMount() {
+    //     axios.get('https://jsonplaceholder.typicode.com/posts')
+    //         .then(res => {
+    //             this.setState({
+    //                 posts: res.data.slice(0, 5)
+    //             })
+    //         })
+    // }
+
     render() {
-        const { posts } = this.state;
+        console.log(this.props);
+        // const { posts } = this.state;
+        const { posts } = this.props;
         const postList = posts.length ? (posts.map(post => {
             return (
                 <div className="post card" key={post.id}>
+
+                    <img src={pokeball} alt="A pokeball" />
                     <div className="card-content">
                         {/* <Link to={'/posts/' + post.id}> */}
                         <Link to={'/' + post.id}>
@@ -35,7 +44,7 @@ class Home extends Component {
                 <div className="center">No posts to show</div>
             )
         return (
-            <div className="container">
+            <div className="container home">
                 <h4 className="center"> Home </h4>
                 {postList}
             </div>
@@ -43,4 +52,13 @@ class Home extends Component {
     }
 }
 
-export default Home
+
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+
+
+export default connect(mapStateToProps)(Home)
+// export default Home
--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer'
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
